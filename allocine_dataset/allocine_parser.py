9fc580b486a4d6165f175e01fd63faf1800f34c7
Théophile Blard <theophile.blard@gmail.com>
2020-01-17 20:40:32 +0100

    better parser script with progress bar and using pandas dataframes

--- allocine_dataset/allocine_parser.py
+++ allocine_dataset/allocine_parser.py
@@ -1,21 +1,23 @@
 import argparse
+from collections import defaultdict
 import json
 import os
-import pickle
+import sys
 
 from bs4 import BeautifulSoup
 import dateparser
+from etaprogress.progress import ProgressBar
+import pandas as pd
 import requests
 
-from common import AllocineReview
-
 ROOT_URL = "http://www.allocine.fr"
-DEFAULT_OUTPUT_PATH = "allocine_dataset/pickle"
+DEFAULT_OUTPUT_PATH = "pickle"
+
 
 def format_text(comment):
     output_text = ""
-    for content in comment.contents:        
-        content_str = str(content)        
+    for content in comment.contents:
+        content_str = str(content)
         content_soup = BeautifulSoup(content_str, 'html.parser')
         spoiler = content_soup.find("span", {"class": "spoiler-content"})
         if spoiler:
@@ -24,114 +26,120 @@ def format_text(comment):
             output_text += content_str.strip()
     return output_text
 
-def parse_page(page_url):    
+
+def parse_page(page_url):
     ratings, reviews, dates, helpfuls = [], [], [], []
     r = requests.get(page_url)
-    soup = BeautifulSoup(r.text, 'html.parser')   
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-    # We iterate on review cards to avoid other reviews (Meilleurs films à l'affiche)
+    # We iterate on reviews to avoid other reviews (Meilleurs films à l'affiche)
     for rating_parent in soup.find_all("div", {"class": "review-card-review-holder"}):
         rating_raw = rating_parent.find("span", {"class": "stareval-note"})  # <span class="stareval-note">4,0</span>
-        rating_str = str(rating_raw.contents)[2:5] # "4,0"
-        rating = float(rating_str.replace(',', '.')) # 4.0        
+        rating_str = str(rating_raw.contents)[2:5]  # "4,0"
+        rating = float(rating_str.replace(',', '.'))  # 4.0
         ratings.append(rating)
 
-    for review_raw in soup.find_all("div", attrs={"class": "content-txt review-card-content"}):        
-        review_text = format_text(review_raw)       
-        reviews.append(review_text) 
+    for review_raw in soup.find_all("div", attrs={"class": "content-txt review-card-content"}):
+        review_text = format_text(review_raw)
+        reviews.append(review_text)
 
     for date_raw in soup.find_all("span", attrs={"class": "review-card-meta-date light"}):
-        date_str = date_raw.text.strip() # Publiée le 24 mai 2011
-        date_str = date_str[11:] # 24 mai 2011
-        date = dateparser.parse(date_str).date() # 2011-05-24
+        date_str = date_raw.text.strip()  # Publiée le 24 mai 2011
+        date_str = date_str[11:]  # 24 mai 2011
+        date = dateparser.parse(date_str).date()  # 2011-05-24
         dates.append(date)
 
     for helpful_raw in soup.find_all("div", {"class": "reviews-users-comment-useful js-useful-reviews"}):
-        helpful_str = helpful_raw.get("data-statistics") # "{"helpfulCount":21,"unhelpfulCount":0}"
-        helpful_dic = json.loads(helpful_str) # {"helpfulCount": 21, "unhelpfulCount": 0}
-        helpful = [helpful_dic["helpfulCount"], helpful_dic["unhelpfulCount"]] # [21, 0]        
-        helpfuls.append(helpful)    
+        helpful_str = helpful_raw.get("data-statistics")  # "{"helpfulCount":21,"unhelpfulCount":0}"
+        helpful_dic = json.loads(helpful_str)  # {"helpfulCount": 21, "unhelpfulCount": 0}
+        helpful = [helpful_dic["helpfulCount"], helpful_dic["unhelpfulCount"]]  # [21, 0]
+        helpfuls.append(helpful)
 
-    assert(len(ratings) == len(reviews) == len(dates) == len(helpfuls)) 
+    assert(len(ratings) == len(reviews) == len(dates) == len(helpfuls))
 
     return ratings, reviews, dates, helpfuls
 
+
 def parse_film(film_url):
+    ratings, reviews, dates, helpfuls = [], [], [], []
     r = requests.get(film_url)
     if r.status_code == requests.codes.not_found:
         # if url is not foud : film does not exist
         return None
-    elif len(r.history) > 1: 
+    elif len(r.history) > 1:
         # if there is more than one element in history, the request was redirected
-        # and that means there are no "critiques/spectateurs" page      
+        # and that means there are no "critiques/spectateurs" page
         return None
 
     soup = BeautifulSoup(r.text, 'html.parser')
-    
-    #print("> Film url: " + film_url)
-    
+
+    # print("> Film url: " + film_url)
+
     # Find number of pages
     pagination = soup.find("div", {"class": "pagination-item-holder"})
+    page_number = 1
     if pagination:
-        #print(pagination)
         pages = pagination.find_all("span")
-        #print([page.text for page in pages])
         page_number = int([page.text for page in pages][-1])
-    else:
-        page_number = 1
-    #print("  pages: " + str(page_number))
-    
-    # Iterate over pages
-    out_reviews = [] #defaultdict(tuple)
 
-    for page_id in range(0, page_number):            
-        page_url = url = "{film_url}/?page={page_num}".format(
+    # print("  pages: " + str(page_number))
+
+    # Iterate over pages
+    for page_id in range(0, page_number):
+        page_url = "{film_url}/?page={page_num}".format(
             film_url=film_url,
             page_num=page_id+1)
-        ratings, reviews, dates, helpfuls = parse_page(page_url)
-        
-        for rating, review_text, date, helpful in zip(ratings, reviews, dates, helpfuls):
-            allocine_review = AllocineReview(
-                rating, 
-                review_text, 
-                date, 
-                helpful
-            )
-            out_reviews.append(allocine_review)            
-    return out_reviews
-            
-    # TODO => only keep x comments (random ?)
+        p_ratings, p_reviews, p_dates, p_helpfuls = parse_page(page_url)
 
-def parse_allocine(root_url, start_id, max_id):   
-    #FILM_ID = 22092
-    reviews = {}
+        ratings.extend(p_ratings)
+        reviews.extend(p_reviews)
+        dates.extend(p_dates)
+        helpfuls.extend(p_helpfuls)
+
+    return (ratings, reviews, dates, helpfuls)
+
+
+def parse_allocine(root_url, start_id, max_id):
+    allocine_dic = defaultdict(list)
+    bar = ProgressBar(max_id-start_id+1, max_width=40)
     for film_id in range(start_id, max_id+1):
-        
-        if film_id % 100 == 0:
-            print("Progress: {}/{}".format(film_id, max_id))
-            
-        film_url = "{root}/film/fichefilm-{film_id}/critiques/spectateurs".format(
+
+        bar.numerator = film_id - start_id
+        print(bar, end='\r')
+        sys.stdout.flush()
+
+        film_url = "{root}/film/fichefilm-{id}/critiques/spectateurs".format(
             root=root_url,
-            film_id=film_id
+            id=film_id
         )
-        film_reviews = parse_film(film_url)
-        if film_reviews:
-            reviews[film_id] = film_reviews
-    
-    return reviews
+
+        parse_output = parse_film(film_url)
+
+        if parse_output:
+            ratings, reviews, dates, helpfuls = parse_output
+            allocine_dic['film-id'].extend(len(ratings)*[film_id])
+            allocine_dic['rating'].extend(ratings)
+            allocine_dic['review'].extend(reviews)
+            allocine_dic['date'].extend(dates)
+            allocine_dic['helpful'].extend([h[0] for h in helpfuls])
+            allocine_dic['unhelpful'].extend([h[1] for h in helpfuls])
+
+    return allocine_dic
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root-url', type=str, default=ROOT_URL)
 parser.add_argument('--output', type=str, default=DEFAULT_OUTPUT_PATH)
-parser.add_argument('--start-id', type=int, default=0)
-parser.add_argument('--max-id', type=int, required=True)
+parser.add_argument('--start-id', type=int, default=1)
+parser.add_argument('--end-id', type=int, required=True)
 args = parser.parse_args()
 
-if __name__ == '__main__':    
-    allocine_reviews = parse_allocine(args.root_url, args.start_id, args.max_id)
-
-    out_file = "allocine_{}_{}.pickle".format(args.start_id, args.max_id)
+if __name__ == '__main__':
+    out_file = "allocine_{}_{}.pickle".format(args.start_id, args.end_id)
     out_path = os.path.join(args.output, out_file)
 
-    with open(out_path, 'wb') as handle:
-        pickle.dump(allocine_reviews, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    allocine_dic = parse_allocine(args.root_url, args.start_id, args.end_id)
+
+    # Create a pandas DataFrame and save it to disk as a picle file
+    df = pd.DataFrame.from_dict(allocine_dic)
+    df.to_pickle(out_path)
